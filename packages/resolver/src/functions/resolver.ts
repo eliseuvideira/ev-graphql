b1099b347bcce6a03035435c4628193bb2879814
Eliseu Videira <eliseuvideira1@gmail.com>
2021-06-28 13:33:01 -0300

    :wrench: added way to replace params

--- packages/resolver/src/functions/resolver.ts
+++ packages/resolver/src/functions/resolver.ts
@@ -1,18 +1,36 @@
-import { GraphQLFieldResolver } from "graphql";
+import { GraphQLResolveInfo } from "graphql";
 
 export type ResolverFn<
   TSource = any,
   TContext = any,
   TArgs = Record<string, any>,
-> = GraphQLFieldResolver<TSource, TContext, TArgs>;
+  TParams = {
+    source?: TSource;
+    args?: TArgs;
+    context?: TContext;
+    info?: GraphQLResolveInfo;
+  },
+> = (
+  source: TSource,
+  args: TArgs,
+  context: TContext,
+  info: GraphQLResolveInfo,
+  params?: TParams,
+) => any;
 
 export const resolver =
   <TSource = any, TContext = any, TArgs = Record<string, any>>(
     ...fns: ResolverFn<TSource, TContext, TArgs>[]
   ): ResolverFn<TSource, TContext, TArgs> =>
-  async (source, args, ctx, info) => {
+  async (source, args, ctx, info, params = {}) => {
     for (const fn of fns) {
-      const value = await fn(source, args, ctx, info);
+      const value = await fn(
+        params.source || source,
+        params.args || args,
+        params.context || ctx,
+        params.info || info,
+        params,
+      );
       if (value != null) {
         return value;
       }

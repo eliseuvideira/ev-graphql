79160e26bf1903d0809910f9577e0ec5c9d63f62
Eliseu Videira <eliseuvideira1@gmail.com>
2021-07-01 16:33:17 -0300

    :wrench: reverting previous change

--- packages/resolver/src/functions/resolver.ts
+++ packages/resolver/src/functions/resolver.ts
@@ -1,36 +1,18 @@
-import { GraphQLResolveInfo } from "graphql";
+import { GraphQLFieldResolver } from "graphql";
 
 export type ResolverFn<
   TSource = any,
   TContext = any,
   TArgs = Record<string, any>,
-  TParams = {
-    source?: TSource;
-    args?: TArgs;
-    context?: TContext;
-    info?: GraphQLResolveInfo;
-  },
-> = (
-  source: TSource,
-  args: TArgs,
-  context: TContext,
-  info: GraphQLResolveInfo,
-  params?: TParams,
-) => any;
+> = GraphQLFieldResolver<TSource, TContext, TArgs>;
 
 export const resolver =
   <TSource = any, TContext = any, TArgs = Record<string, any>>(
     ...fns: ResolverFn<TSource, TContext, TArgs>[]
   ): ResolverFn<TSource, TContext, TArgs> =>
-  async (source, args, ctx, info, params = {}) => {
+  async (source, args, ctx, info) => {
     for (const fn of fns) {
-      const value = await fn(
-        params.source || source,
-        params.args || args,
-        params.context || ctx,
-        params.info || info,
-        params,
-      );
+      const value = await fn(source, args, ctx, info);
       if (value != null) {
         return value;
       }
